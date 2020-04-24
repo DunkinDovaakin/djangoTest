a5a25b1d1581f858ed55f56a389b067ee7763afb
Ilya Finagentov <ilyafinagentov@MacBook-Pro-Ilya.local>
2020-04-24 21:21:48 +0300

    init

---
+++ kaf42.pw/django_test/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ilyafinagentov/django_projects/djangoTest/kaf42.pw/django_test/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
